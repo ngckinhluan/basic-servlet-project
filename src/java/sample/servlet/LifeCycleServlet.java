e3364a06d79e60d57bfef9632a971136365e9f1f
TranLuan2907 <nkluantran2907@gmail.com>
2024-01-13 12:11:09 +0700

    first commit

---
+++ src/java/sample/servlet/LifeCycleServlet.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author LuanTNKSE184059
+ */
+@WebServlet(name = "LifeCycleServlet", urlPatterns = {"/LifeCycleServlet"})
+public class LifeCycleServlet extends HttpServlet {
+    private int a = 0;
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("init");
+        a += 5;
+        System.out.println("a = " + a);
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Life Cycle</h1>");
+            
+            
+            a += 10;
+            out.println("a = " + a);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        System.out.println("doGet is invoked!");
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        System.out.println("doPost is invoked!");
+    }
+    
+//    @Override
+//    protected void service(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        System.out.println("service");
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        out.println("This is service");
+//    }
+    
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("Destroy");
+    }
+
+}
